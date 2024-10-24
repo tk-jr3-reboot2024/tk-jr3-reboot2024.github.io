dfa4b49b1ad7ede807007292f1dc60196f10567e
kinugoshi8928 <kappagloop@gmail.com>
2024-10-24 23:20:19 +0900

    直った なんでpタグ使ってるんですか??????????????

--- src/components/introduction/3A.js
+++ src/components/introduction/3A.js
@@ -2,9 +2,9 @@ import React from "react";
 import Content from "./3a-components/Content";
 
 export default function A() {
-  return (
-    <p>
-      <Content />
-    </p>
-  );
+    return (
+        <div>
+            <Content />
+        </div>
+    );
 }
