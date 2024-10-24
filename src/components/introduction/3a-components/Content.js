63ee1ff603ce3b55ece2977e90a5e5b103c04fea
kinugoshi8928 <kappagloop@gmail.com>
2024-10-25 01:21:05 +0900

    ヘッダーをなんとかした

--- src/components/introduction/3a-components/Content.js
+++ src/components/introduction/3a-components/Content.js
@@ -1,26 +1,45 @@
 import React from "react";
-import { Box, CssBaseline } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import toplogo from "./img/PamphletLogo.jpg";
+import theme from "./theme";
 
 export default function Content() {
     return (
         <div>
-            <CssBaseline /> {/* CSSリセット */}
-            <main style={{ padding: 0, marginTop: 0 }}>
-                <Box
-                    component="img"
-                    src={toplogo}
-                    alt="toplogo"
-                    sx={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
-                        display: "block",
-                        margin: 0,
-                        padding: 0,
-                    }}
-                ></Box>
-            </main>
+            <ThemeProvider theme={theme}>
+                <CssBaseline /> {/* CSSリセット */}
+                <main style={{ padding: 0, marginTop: 0 }}>
+                    <Box
+                        component="img"
+                        src={toplogo}
+                        alt="toplogo"
+                        sx={{
+                            width: "100%",
+                            height: "100%",
+                            objectFit: "cover",
+                            display: "block",
+                            margin: 0,
+                            padding: 0,
+                        }}
+                    ></Box>
+                </main>
+                <main>
+                    <p>
+                        日常には、小さなデスゲームがいくつも潜んでいる。
+                        <br />
+                        ここは、月見大学附属かまぼこ中学校。
+                        <br />
+                        課題やテストなど、様々な試練が生徒達を待ち受ける。
+                        <br />
+                        間違えてはいけない、バレてはいけない。
+                        <br />
+                        誰が味方で、誰が敵かもわからない。
+                        <br />
+                        デスゲームは、きっとあなたのそばにも。
+                        <br />
+                    </p>
+                </main>
+            </ThemeProvider>
         </div>
     );
 }
--- src/components/introduction/3a-components/Header.js
+++ src/components/introduction/3a-components/Header.js
@@ -1,36 +1,40 @@
 import React from "react";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
+import { AppBar, Toolbar, Typography, Button, IconButton } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <AppBar
-            position="static"
-            color="primary"
-            sx={{ padding: 0, margin: 0 }}
-        >
+        <AppBar position="static" color="primary">
             <Toolbar>
                 <IconButton
                     edge="start"
                     color="inherit"
-                    aria-label="menu"
-                    sx={{ mr: 2 }}
+                    aria-label="home"
+                    component={Link}
+                    to="/"
                 >
-                    <MenuIcon />
+                    <HomeIcon />
                 </IconButton>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    My Website
+                <Typography
+                    variant="h6"
+                    component={Link}
+                    to="/introduction/a"
+                    sx={{
+                        flexGrow: 1,
+                        textDecoration: "none",
+                        color: "inherit",
+                    }}
+                >
+                    School Life
                 </Typography>
-                <Box>
-                    <Button color="inherit">Home</Button>
-                    <Button color="inherit">About</Button>
-                    <Button color="inherit">Contact</Button>
-                </Box>
+                <Button
+                    color="inherit"
+                    component={Link}
+                    to="/introduction/a/access"
+                >
+                    アクセス
+                </Button>
             </Toolbar>
         </AppBar>
     );
