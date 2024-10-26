4c0d1e0e4e9fc04f06d1a20ac32111268a720eec
tfl_tkpc <ryo10.tfl@icloud.com>
2024-10-26 21:14:55 +0900

    changed content.js introduction paragraph

--- src/components/introduction/3c-components/Content.js
+++ src/components/introduction/3c-components/Content.js
@@ -21,10 +21,13 @@ function Content() {
 			}}
 		/>
 		</main>
-		<main style={{ padding: '2vw', marginLeft: '2vw', color: 'white'}}>
+		<main style={{ padding: '2vw', marginLeft: '2vw', color: 'black/*white*/'}}>
 			<h1 style={{fontFamily: 'serif', fontSize: '40px'}}>Story</h1>
 			<p>
-				ほげほげ
+			国内でも唯一の国立男子校、筑波大学附属駒場中高、通称・筑駒。
+			ある日、そんな筑駒に麻布麗という生徒が転入してきた。
+			容姿端麗で学問・スポーツ共に優れた麗は、やがてこの学校の独特な雰囲気に疑問を覚えるようになり、
+			生徒たちをそのカリスマでまとめ上げ、改革をしようとしていくが…？
 			</p>
 		</main>
 		<Box sx={{padding: '2vw'}}>
