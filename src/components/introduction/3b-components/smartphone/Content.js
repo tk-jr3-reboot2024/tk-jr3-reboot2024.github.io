6a0f0d2c908041131d9574d8044db2a3b6908bd9
highlighter-math <140462663+highlighter-math@users.noreply.github.com>
2024-10-29 02:00:46 +0900

    Create Content.js

---
+++ src/components/introduction/3b-components/smartphone/Content.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Box, CssBaseline} from '@mui/material';
+
+function Content() {
+	return (
+		<div>
+		<CssBaseline />
+		<main style={{ padding: '2vw', marginLeft: '2vw', color: 'white'}}>
+			<h1 style={{fontFamily: 'serif', fontSize: '40px'}}>Story</h1>
+			<p>
+				ほげほげ
+			</p>
+		</main>
+		</div>
+	);
+};
+
+export default Content;
