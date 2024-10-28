3a4d23c9fdaac4d4c3e6cf44663dffc8efc38238
tfl_tkpc <ryo10.tfl@icloud.com>
2024-10-28 14:26:41 +0900

    made navbar

--- src/components/introduction/3c-components/Header.js
+++ src/components/introduction/3c-components/Header.js
@@ -1,89 +1,56 @@
-import React, { useState } from 'react';
+import React from 'react';
 //import './Header.css'; // CSSを適用するファイルをインポート
-import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
-import { Link, useLocation } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem, Link } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import HomeIcon from '@mui/icons-material/Home';
 import Box from '@mui/material/Box';
+import { green } from '@mui/material/colors'
 
-const Header = () => {
-	const location = useLocation();
-	const [hoveredItem, setHoveredItem] = useState(null);
-	const menuItems = ['Home', 'News', 'Photos'];
-	const currentPath = location.pathname;
-	const getLink = (item) => {
-		switch (item) {
-			case 'Home':
-				return '/introduction/c';
-			case 'News':
-				return '/introduction/c/news';
-			case 'Photos':
-				return '/introduction/c/photo';
-			default:
-				return '/introduction/c';
-		}
+function BasicMenu() {
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
 	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
 	return (
 		<div>
-			<AppBar position="static" sx={{ backgroundColor: '#222222', color: '#fff', padding: '0px' }}>
-				<Toolbar sx={{ padding: '10px' }}>
-					<IconButton component={Link} to="/" sx={{ color: 'inherit', padding: '0', minWidth: 'auto', width: '5vw', mr: '0px', ml: '0px', display: 'flex', justifyContent: 'center', '&:hover': { color: '#ffbe00' } }}>
-						<HomeIcon sx={{ fontSize: 'min(40px,max(20px,3vw))' }} />
+			<AppBar position="fixed">
+				<Toolbar>
+					<IconButton
+						id="basic-button"
+						aria-controls={open ? 'basic-menu' : undefined}
+						aria-haspopup="true"
+						aria-expanded={open ? 'true' : undefined}
+						onClick={handleClick}
+						size="large"
+						sx={{ color: green[500] }}
+					>
+						<MenuIcon />
 					</IconButton>
-					<Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'space-between', ml: 0 }}>
-						{menuItems.map((item, index) => {
-							const isActive = getLink(item) === currentPath;
-							const isHovered = hoveredItem === item;
-							return (
-								<Box
-									key={index}
-									component={Link}
-									to={getLink(item)}
-									onMouseEnter={() => setHoveredItem(item)}
-									onMouseLeave={() => setHoveredItem(null)}
-									sx={{
-										display: 'flex',
-										justifyContent: 'center',
-										alignItems: 'center',
-										flexGrow: 1,
-										position: 'relative',
-										textDecoration: 'none',
-										color: 'inherit',
-										paddingBottom: '5px',
-										'&:hover': {
-											color: '#ffbe00'
-										}
-									}}
-								>
-									<Typography variant="h5" component="div"
-										sx={{
-											textDecoration: 'none',
-											color: 'inherit',
-											position: 'relative',
-											fontFamily: 'serif',
-											fontSize: 'max(2vw,20px)'
-										}}
-									>
-										{item}
-									</Typography>
-									<Box
-										sx={{
-											height: '2px',
-											width: isActive || isHovered ? '80%' : '0%',
-											backgroundColor: '#ffbe00',
-											position: 'absolute',
-											bottom: 0,
-											left: '10%',
-											transition: 'width 0.3s ease'
-										}}
-									/>
-								</Box>
-							);
-						})}
-					</Box>
+					<Menu
+						id="basic-menu"
+						anchorEl={anchorEl}
+						open={open}
+						onClose={handleClose}
+						MenuListProps={{
+							'aria-labelledby': 'basic-button',
+						}}
+					>
+						<MenuItem onClick={handleClose}><Link href="/" underline="none">中3HPトップ</Link></MenuItem>
+						<MenuItem onClick={handleClose}><Link href="/" underline="none">3-CHRトップ</Link></MenuItem>
+						<MenuItem onClick={handleClose}><Link href="/" underline="none">公演について</Link></MenuItem>
+					</Menu>
+					<Typography variant="h6" noWrap>
+						翔んで筑駒
+					</Typography>
 				</Toolbar>
 			</AppBar>
 		</div>
 	);
-};
+}
 
-export default Header;
+export default BasicMenu;
