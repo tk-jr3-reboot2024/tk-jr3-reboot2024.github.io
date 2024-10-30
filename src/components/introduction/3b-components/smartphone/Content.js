6957fd068e7558aa57585c5b105a47097cb72378
highlighter-math <140462663+highlighter-math@users.noreply.github.com>
2024-10-30 23:33:10 +0900

    Update Content.js

--- src/components/introduction/3b-components/smartphone/Content.js
+++ src/components/introduction/3b-components/smartphone/Content.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {Box, Typography, CssBaseline, ThemeProvider} from '@mui/material';
+import {Box, Typography, CssBaseline, ThemeProvider, TextField} from '@mui/material';
 import { motion, AnimatePresence } from 'framer-motion';
 import Recreate_logo from './img/Recreate_logo_negate.png';
 import Recreate_subtitle from './img/Recreate_subtitle_negate.png';
@@ -46,29 +46,94 @@ function Content() {
 			animate={{ opacity: 1 }}
 			transition={{ duration: 1 }}
 			>
-
-
-		<Box display="flex" justifyContent="center" alignItems="center" height="auto" padding="20px">
-			<img
-			src={Recreate_logo} // 画像のパスを指定
-			alt="mainlogo"
-			style={{ width: "80%", height: "auto", borderRadius: "10px" }}
-			/>
+			<Box display="flex" justifyContent="center" alignItems="center" height="auto" padding="20px">
+				<img
+				src={Recreate_logo} // 画像のパスを指定
+				alt="mainlogo"
+				style={{ width: "80%", height: "auto", borderRadius: "10px" }}
+				/>
+			</Box>
+			<Box display="flex" justifyContent="center" alignItems="center" height="auto">
+				<img
+				src={Recreate_subtitle} // 画像のパスを指定
+				alt="mainlogo"
+				style={{ width: "50%", height: "auto", borderRadius: "10px" }}
+				/>
+			</Box>
+			<Box
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				mt: 3,
+				position: 'relative'
+			}}
+			>
+				<Typography
+				variant="body1"
+				sx={{
+					mb: 1,
+					color: '#fff',
+					fontFamily: 'serif',
+					fontSize: '15px'
+				}}
+				>
+					SCROLL
+				</Typography>
+				<Box
+				sx={{
+					width: '60px',
+					height: '60px',
+					borderRadius: '50%',
+					border: '1px solid #fff',
+					position: 'relative',
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center'
+				}}
+				/>
+				<Box
+				sx={{
+					position: 'absolute',
+					bottom: 'calc(100% - 30px + 120px)',
+					top: 'calc(100% - 30px)',
+					height: '120px',
+					width: '1px',
+					overflow: 'hidden'
+				}}
+				>
+				<motion.div
+				initial={{top: '-120px' }}
+				animate={{top: '120px' }}
+				transition={{
+					duration: 2.0,
+					ease: 'easeInOut',
+					repeat: Infinity,
+					repeatType: 'loop',
+				}}
+				style={{
+					width: '1px',
+					backgroundColor: '#fff',
+					position: 'absolute',
+					bottom: '0',
+					height: '120px'
+				}}
+				/>
+				</Box>
 		</Box>
-		<Box display="flex" justifyContent="center" alignItems="center" height="auto">
-			<img
-			src={Recreate_subtitle} // 画像のパスを指定
-			alt="mainlogo"
-			style={{ width: "50%", height: "auto", borderRadius: "10px" }}
-			/>
-		</Box>
-		<main style={{ marginLeft: '4vw', marginTop: '20vw' , color: 'white'}}>
+		<Box
+		sx={{
+			marginLeft: '20px',marginTop: '150px', color: '#fff'
+		}}
+		>
 			<h1 style={{fontFamily: 'serif', fontSize: '40px'}}>Story</h1>
 			<p>
 				ほげほげ
 			</p>
 			<br />
-		</main>
+		</Box>
+		
+
 		</motion.div>
 		)}
 		</Box>
