243bae527dd856ececfccce68bbfc7a6470e929a
tfltkpc <ryo10.tfl@icloud.com>
2024-10-30 04:02:56 +0000

    created c/news/1.js

---
+++ src/components/introduction/3c-components/news/1.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Typography, Box, CssBaseline, Card, CardActionArea, CardContent, Chip, Divider, ThemeProvider, CardActions, Button } from '@mui/material';
+import Grid from '@mui/material/Grid2';
+import { grey } from '@mui/material/colors';
+import theme from "./theme";
+
+function News1() {
+	return (
+		<div>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<Box sx={{ padding: '2vw', box_width: '100%' }}>
+					hello
+				</Box>
+			</ThemeProvider>
+		</div>
+	);
+}
+
+export default News1
