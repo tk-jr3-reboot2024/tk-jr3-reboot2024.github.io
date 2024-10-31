ae6d6e27d1a9ac8810c3951b333c56bd387891b9
highlighter-math <140462663+highlighter-math@users.noreply.github.com>
2024-10-31 17:52:44 +0900

    Update Content.js

--- src/components/introduction/3b-components/smartphone/Content.js
+++ src/components/introduction/3b-components/smartphone/Content.js
@@ -4,6 +4,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import Recreate_logo from './img/Recreate_logo_negate.png';
 import Recreate_subtitle from './img/Recreate_subtitle_negate.png';
 import theme from "./theme";
+import Seiriken from './seiriken';
 
 function Content() {
 	const [isImageVisible, setIsImageVisible] = useState(true);
@@ -124,13 +125,57 @@ function Content() {
 		</Box>
 		<Box
 		sx={{
-			marginLeft: '20px',marginTop: '150px', color: '#fff'
+			marginLeft: '48px',marginRight: '48px',paddingBottom: '48px' ,marginTop: '150px', color: '#fff'
 		}}
 		>
-			<h1 style={{fontFamily: 'serif', fontSize: '40px'}}>Story</h1>
-			<p>
-				ほげほげ
-			</p>
+			<Box
+			sx={{
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				flexDirection: 'column',
+				color: '#fff'
+			}}
+			>
+			<Typography variant="h6" sx={{fontFamily: 'serif', fontSize: '40px'}}>
+				Story
+			</Typography>
+			</Box>
+			<Typography variant="body1" sx={{fontFamily: 'serif', fontSize: '20px'}}>
+				今日は待ちに待った文化祭。
+			</Typography>
+			<Typography variant="body1" sx={{fontFamily: 'serif', fontSize: '20px'}}>
+			あまりの多忙でクラスデコの準備は手伝えなかったが、本番のシフトでは全力を尽くそうと考えていた。
+			</Typography>
+			<Typography variant="body1" sx={{fontFamily: 'serif', fontSize: '20px'}}>
+				楽しみで少し早く教室に到着し、扉を開けた瞬間―あなたは自分の目を疑った。
+			</Typography>
+			<Typography variant="body1" sx={{fontFamily: 'serif', fontSize: '20px'}}>
+				散乱した木材、剝がされたパネル、乱雑な机…
+			</Typography>
+			<Typography variant="body1" sx={{fontFamily: 'serif', fontSize: '20px'}}>
+				全てが、破壊されていた。
+			</Typography>
+			<Typography variant="body1" sx={{fontFamily: 'serif', fontSize: '20px'}}>
+				あなたは慌てて文化祭実行委員を呼び、散らかった教室の片づけを手伝ってもらったのだが、彼らにも破壊さたデコを修復することは出来ない。
+			</Typography>
+			<Typography variant="body1" sx={{fontFamily: 'serif', fontSize: '20px'}}>
+				絶望しかけたその時、誰かの日記が入ったファイルが見つかったのだった―
+			</Typography>
+		</Box>
+		<Box
+		sx={{
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'center',
+			flexDirection: 'column',
+			color: '#fff'
+		}}
+		>
+			<Typography variant="h6" sx={{fontFamily: 'serif', fontSize: '30px'}}>
+				次回の整理券配布
+			</Typography>
+			<Seiriken />
 			<br />
 		</Box>
 		
