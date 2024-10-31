76af9725b9e1794360dd96b6e094f0c14c7eeb2a
tfl_tkpc <ryo10.tfl@icloud.com>
2024-11-01 07:29:30 +0900

    google search console

--- src/components/introduction/3c-components/tsukukoma.js
+++ src/components/introduction/3c-components/tsukukoma.js
@@ -17,9 +17,18 @@ function News1() {
                         <i>Introduction to Tsukukoma</i>
                     </Typography>
                     <Divider sx={{ margin: '8px' }} />
-                    <Typography variant='h6'>【WIP】</Typography>
+                    <Typography variant='h6'>【はじめに】</Typography>
                     <Typography variant="p">
-                        Wip
+                        「翔んで筑駒」はすべて筑駒にて撮影されており、筑駒の学校生活を映し出す場面を数多く含めています。
+                        <br />
+                        そこで、このページでは普段聞くことができない「ありのままの筑駒」の学校生活について、紹介していきたいと思います。
+                    </Typography>
+                    <Divider sx={{ margin: '8px' }} />
+                    <Typography variant='h6'>【授業の様子】</Typography>
+                    <Typography variant="p">
+                        　日本で唯一の国立男子校である筑駒では、学習指導要領に沿っていながらも特色のある授業が行われています。
+                        <br />
+                        そこで、このページでは普段聞くことができない「ありのままの筑駒」の学校生活について、紹介していきたいと思います。
                     </Typography>
                 </Box>
             </ThemeProvider>
