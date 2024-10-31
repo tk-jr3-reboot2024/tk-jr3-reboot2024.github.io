8271aaad8890c9335d1e52784981ced4207d24de
tfl_tkpc <ryo10.tfl@icloud.com>
2024-11-01 06:40:33 +0900

    story.js

--- src/components/introduction/3c-components/story.js
+++ src/components/introduction/3c-components/story.js
@@ -17,9 +17,53 @@ function News1() {
                         <i>Story & Cast</i>
                     </Typography>
                     <Divider sx={{ margin: '8px' }} />
-                    <Typography variant='h6'>【WIP】</Typography>
+                    <Typography variant='h6'>【ストーリー】</Typography>
                     <Typography variant="p">
-                        WIP
+                        国内でも唯一の国立男子校、筑波大学附属駒場中高、通称・筑駒。
+                        <br />
+                        ある日、そんな筑駒に麻生麗という生徒が転入してきた。
+                        <br />
+                        容姿端麗で学問・スポーツ共に優れた麗は、やがてこの学校の独特な雰囲気に疑問を覚えるようになり、
+                        <br />
+                        生徒たちをそのカリスマでまとめ上げ、改革をしようとしていくが…?
+                        <br />生徒会軍と麻生軍の対立や、「ありのままの筑駒」の描写をぜひお楽しみください。
+                    </Typography>
+                    <Divider sx={{ margin: '8px' }} />
+                    <Typography variant='h6'>【キャスト】</Typography>
+                    <Typography variant="p">
+                        ※個人情報保護の観点から、ホームページ上でのキャスト本名公開は控えさせていただきます。
+                        <br />
+                        ＜「筑駒モダン化計画」軍＞
+                        <br />
+                        麻生 麗
+                        <br />
+                        伊藤 尚子
+                        <br />
+                        永会 和太郎
+                        <br />
+                        カワウソ
+                        <br />
+                        蓮見 翔
+                        <br />
+                        醍醐
+                        <br />
+                        その他
+                        <br />
+                        ＜「ありのままの筑駒」側＞
+                        <br />
+                        仁井 三太郎
+                        <br />
+                        一色 緑
+                        <br />
+                        金 政宗
+                        <br />
+                        古佐 太郎
+                        <br />
+                        野比 のぎ太
+                        <br />
+                        井瀬果 伊月
+                        <br />
+                        その他
                     </Typography>
                 </Box>
             </ThemeProvider>
