b4ff71f245e2d35b3c0931fa3d09cff1b37d9a59
tfl_tkpc <ryo10.tfl@icloud.com>
2024-11-01 09:10:47 +0900

    reverse

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,15 +2,10 @@ import React from 'react'
 
 function Content() {
 	return (
-		<React.Fragment>
-		<head>
-		<meta name="google-site-verification" content="PoE5YMbNyuHbvXzDHANQ2VA0jTxN8UgbBo8qnQPIowY" />
-		</head>
 		<p>
 			2024年筑駒文化祭、中3HRデコ合同ホームページへようこそ!<br></br>
 			左上のメニューから各クラスのデコHPに飛ぶことができます。
 		</p>
-		</React.Fragment>
 	);
 }
 
