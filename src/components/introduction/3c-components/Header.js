b34e60a8fb24b7ff2a992589f60e90e55641f3d8
tfltkpc <ryo10.tfl@icloud.com>
2024-11-03 13:56:28 +0900

    Update Header.js

--- src/components/introduction/3c-components/Header.js
+++ src/components/introduction/3c-components/Header.js
@@ -46,6 +46,7 @@ function BasicMenu() {
 						<MenuItem onClick={handleClose}><Link href="/introduction/c/news" underline="none" sx={{ color: grey[900] }} >ニュース</Link></MenuItem>
 						<MenuItem onClick={handleClose}><Link href="/introduction/c/story" underline="none" sx={{ color: grey[900] }} >ストーリーとキャスト</Link></MenuItem>
 						<MenuItem onClick={handleClose}><Link href="/introduction/c/tsukukoma" underline="none" sx={{ color: grey[900] }} >「ありのままの筑駒」学校紹介</Link></MenuItem>
+						<MenuItem onClick={handleClose}><Link href="/introduction/c/making" underline="none" sx={{ color: grey[900] }} >映画製作の裏側</Link></MenuItem>
 						<MenuItem onClick={handleClose}><Link href="/introduction/c/access" underline="none" sx={{ color: grey[900] }} >アクセス</Link></MenuItem>
 					</Menu>
 					<Typography variant="h6" noWrap>
@@ -63,5 +64,5 @@ export default BasicMenu;
 
 					
 
-						<MenuItem onClick={handleClose}><Link href="/introduction/c/making" underline="none" sx={{ color: grey[900] }} >映画製作の裏側</Link></MenuItem>
+						
 */
